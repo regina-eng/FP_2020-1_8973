378feb14f4a8f6093854d644bbf201c3a493312d
regina-eng <54340677+regina-eng@users.noreply.github.com>
2019-10-22 02:00:34 +0200

    Tarea 3

---
+++ TAREAS/3/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (int argc, char *argu[]){
+	float n1,n2;
+	n1= atof(argu[1]);
+	n2= atof(argu[2]);
+	printf("%.2f\n",n1+n2);
+	printf("%.2f\n",n1-n2);
+	printf("%.2f\n",n1*n2);
+	printf("%.2f\n",n1/n2);
+	return 0;
+
+}
